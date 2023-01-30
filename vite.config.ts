44e9f08fcff318e519f93e95b678edf18b8ae896
橋本 龍 <hashimotoryoh@gmail.com>
2023-01-30 18:02:59 +0900

    unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Pages(),
+    AutoImport({
+      include: [
+        /\.ts$/,
+        /\.tsx$/,
+        /\.js$/,
+        /\.jsx$/,
+        /\.vue$/
+      ],
+      imports: [
+        'vue',
+      ],
+      dts: './src/auto-imports.d.ts',
+    }),
   ],
 })
