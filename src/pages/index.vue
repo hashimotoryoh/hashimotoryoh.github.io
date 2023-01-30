05c7d2fc2c1820f252883cafa77f092c326996de
橋本 龍 <hashimotoryoh@gmail.com>
2023-01-30 18:18:09 +0900

    プロフィールアイコン表示

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,15 +3,15 @@ import Link from '../types/Link'
 
 const links: Link[] = [
   {
-    url: import.meta.env.LINK_FACEBOOK,
+    url: import.meta.env.VITE_LINK_FACEBOOK,
     icon: 'mdi:facebook',
   },
   {
-    url: import.meta.env.LINK_INSTAGRAM,
+    url: import.meta.env.VITE_LINK_INSTAGRAM,
     icon: 'mdi:instagram',
   },
   {
-    url: import.meta.env.LINK_TWITTER,
+    url: import.meta.env.VITE_LINK_TWITTER,
     icon: 'mdi:twitter',
   },
 ]
@@ -19,16 +19,17 @@ const links: Link[] = [
 
 <template>
   <div class="container mx-auto py-4">
-    <div class="text-center">
-      <!-- <img src="/img/profile.png" srcset="/img/profile.png 1x, /img/profile@2x.png 2x, /img/profile@3x.png 3x" alt="Ryo Hashimoto" /> -->
+    <div class="mb-8 text-center">
+      <img src="/img/profile.png" srcset="/img/profile.png 1x, /img/profile@2x.png 2x, /img/profile@3x.png 3x" alt="Ryo Hashimoto" width="96" height="96" class="mx-auto mb-4 border rounded-full" />
       <h1 class="h1 font-bold text-xl">Ryo Hashimoto</h1>
     </div>
-    <ul class="flex justify-center space-x-4 mx-auto my-2">
-      <li v-for="link in links" >
-        <a :href="link.url" target="_blank" rel="nofollow">
+    <ul class="flex justify-center space-x-8 mx-auto my-2">
+      <li v-for="link in links">
+        <a :href="link.url" target="_blank" rel="nofollow" class="display text-slate-400 hover:text-black">
           <Icon :icon="link.icon" class="text-4xl" />
         </a>
       </li>
     </ul>
+    <hr class="my-8 mx-2">
   </div>
 </template>
