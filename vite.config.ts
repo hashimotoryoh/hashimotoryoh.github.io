8d6d0f700e887eed96294b87e78d6b9b022a52d2
橋本 龍 <hashimotoryoh@gmail.com>
2023-01-30 18:37:42 +0900

    リポジトリ名を記述

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'hashimotoryoh.github.io',
   plugins: [
     vue(),
     Pages(),
