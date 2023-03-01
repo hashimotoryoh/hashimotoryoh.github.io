caf7e03e6960340e3fc35d913c3567f16eaaa08c
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-01 08:56:36 -0800

    note のリンクを追加
    パスで始まる場合は img タグを使う

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,6 +19,10 @@ const links: Link[] = [
     url: config.sns.twitter_url,
     icon: 'mdi:twitter',
   },
+  {
+    url: config.sns.note_url,
+    icon: '/img/note/square.svg',
+  },
 ]
 </script>
 
@@ -31,7 +35,8 @@ const links: Link[] = [
     <ul class="flex justify-center space-x-8 mx-auto my-2">
       <li v-for="link in links">
         <a :href="link.url" target="_blank" rel="nofollow" class="display text-slate-400 rounded hover:text-black">
-          <Icon :icon="link.icon" class="text-4xl" />
+          <img v-if="link.icon.startsWith('http') || link.icon.startsWith('/')" :src="link.icon" class="w-9" />
+          <Icon v-else :icon="link.icon" class="text-4xl" />
         </a>
       </li>
     </ul>
