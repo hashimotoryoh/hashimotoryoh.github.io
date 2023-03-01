90ce601a4260a28bbafaad6253950d9c65a2e335
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-01 09:23:58 -0800

    tailwind の読み込みも app で行う

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/styles/tailwind.scss'
 import './assets/styles/app.scss'
 import { Icon } from '@iconify/vue'
 
