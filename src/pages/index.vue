236fdac7ed377808dce209c5f2c51c8b81bbb6cc
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-01 09:28:45 -0800

    リンクにタイトルを付与

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,22 +4,27 @@ import config from '../config'
 
 const links: Link[] = [
   {
+    name: 'Email',
     url: 'mailto:' + config.profile.email,
     icon: 'mdi:email',
   },
   {
+    name: 'Facebook',
     url: config.sns.facebook_url,
     icon: 'mdi:facebook',
   },
   {
+    name: 'Instagram',
     url: config.sns.instagram_url,
     icon: 'mdi:instagram',
   },
   {
+    name: 'Twitter',
     url: config.sns.twitter_url,
     icon: 'mdi:twitter',
   },
   {
+    name: 'note',
     url: config.sns.note_url,
     icon: '/img/note/square.svg',
   },
@@ -34,8 +39,8 @@ const links: Link[] = [
     </div>
     <ul class="flex justify-center space-x-8 mx-auto my-2">
       <li v-for="link in links">
-        <a :href="link.url" target="_blank" rel="nofollow" class="display rounded slate hover:filter-none">
-          <img v-if="link.icon.startsWith('http') || link.icon.startsWith('/')" :src="link.icon" class="w-9 border border-black rounded-full" />
+        <a :href="link.url" :title="link.name" target="_blank" rel="nofollow" class="display rounded slate hover:filter-none">
+          <img v-if="link.icon.startsWith('http') || link.icon.startsWith('/')" :src="link.icon" :alt="link.name" class="w-9 border border-black rounded-full" />
           <Icon v-else :icon="link.icon" class="text-4xl" />
         </a>
       </li>
