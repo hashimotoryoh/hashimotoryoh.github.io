8284abeb3fa88baa378957176efcb16d9383604d
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-02 12:39:41 -0800

    やっぱりSSRじゃないとできないので、決めつけで置いておく
    https://github.com/hashimotoryoh/hashimotoryoh.github.io/issues/3

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,6 @@ import config from './config'
 
 useHead({
   title: config.app.app_name,
-  meta: [
-    {
-      name: 'og:title',
-      content: config.app.app_name,
-    },
-    {
-      name: 'og:url',
-      content: config.app.base_url,
-    },
-    {
-      name: 'og:image',
-      content: config.app.base_url + '/og-image.png'
-    }
-  ]
 })
 </script>
 
