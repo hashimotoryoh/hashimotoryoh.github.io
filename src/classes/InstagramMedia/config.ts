4a6b798b1dedb03a84a3f69001315422c114c478
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-01 17:14:45 -0800

    env に移動

--- src/classes/InstagramMedia/config.ts
+++ src/classes/InstagramMedia/config.ts
@@ -1,4 +1,4 @@
 export default {
   endpoint: 'https://graph.instagram.com/me/media',
-  accessToken: 'IGQVJYZAkFFajlLOXpIRlViS1dNTncxWTFjaUc2dHVzMV9Kck5EOTFtVzNLZAVF4dmZAjODl3ZATZA6dWYtTk82RGstRldXXzA5am9NaXpDUTlDYkZAxSTZAhQWg4N1ZAneXdzLVpRbzdNT2hxejR0V1RHUDg4TwZDZD',
+  accessToken: import.meta.env.VITE_INSTAGRAM_APP_ACCESS_TOKEN,
 }
