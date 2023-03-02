bb545cae2be62e51735643b8977d16b8260cf825
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-01 21:08:34 -0800

    instagram いらない

--- src/classes/InstagramMedia/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  endpoint: 'https://graph.instagram.com/me/media',
-  accessToken: import.meta.env.VITE_INSTAGRAM_APP_ACCESS_TOKEN,
-}
--- src/classes/InstagramMedia/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Media {
-  id: string
-  caption?: string
-  media_type: 'IMAGE' | 'CAROUSEL_ALBUM' | 'VIDEO' | string
-  permalink: URL
-  media_url: URL
-}
