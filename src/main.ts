803947eeed4d4f65a874a644ad54c0680f8ac805
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-05 10:30:18 -0800

    cookie を使う準備

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import head from './classes/setup/head'
 import router from './classes/setup/router'
 import i18n from './classes/setup/i18n'
+import VueCookies from 'vue3-cookies'
 import './assets/styles/app.scss'
 import { Icon } from '@iconify/vue'
 
@@ -10,5 +11,8 @@ createApp(App)
   .use(head)
   .use(router)
   .use(i18n)
+  .use(VueCookies, {
+    secure: true,
+  })
   .component('Icon', Icon)
   .mount('#app')
