200a594f51ba0fafa4e9198ec1c8f754c147ad26
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-05 10:44:05 -0800

    ダークモードのオン/オフを cookie に保存する

--- src/classes/utilities/DarkModeUtil.ts
+++ src/classes/utilities/DarkModeUtil.ts
@@ -1,15 +1,20 @@
+import { useCookies } from 'vue3-cookies'
+
 export function useDarkMode() {
   const darkMode = ref<boolean>(false)
   const htmlDOM = document.getElementsByTagName('html')[0]
+  const { cookies } = useCookies()
 
   const turnOn = () => {
     darkMode.value = true
     htmlDOM.classList.add('dark')
+    cookies.set('is_dark_mode', darkMode.value.toString())
   }
 
   const turnOff = () => {
     darkMode.value = false
     htmlDOM.classList.remove('dark')
+    cookies.set('is_dark_mode', darkMode.value.toString())
   }
 
   const switchDarkMode = () => {
@@ -17,7 +22,12 @@ export function useDarkMode() {
   }
 
   onMounted(() => {
-    darkMode.value = htmlDOM.classList.contains('dark')
+    if (cookies.isKey('is_dark_mode')) {
+      darkMode.value = (cookies.get('is_dark_mode') === (true).toString())
+      darkMode.value ? turnOn() : turnOff()
+    } else {
+      darkMode.value = htmlDOM.classList.contains('dark')
+    }
   })
 
   const isDarkMode = readonly(darkMode)
