78d7cf0ef2f3b6df74ff79746212efa3925a58a3
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-03-06 02:14:03 -0800

    ローカライズの設定を cookie に保存

--- src/classes/setup/i18n.ts
+++ src/classes/setup/i18n.ts
@@ -1,10 +1,16 @@
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages'
+import { useCookies } from 'vue3-cookies'
+
+const { cookies } = useCookies()
+const locale = cookies.isKey('i18n_locale')
+  ? cookies.get('i18n_locale')
+  : navigator.language
 
 const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: navigator.language,
+  locale: locale,
   fallbackLocale: 'en',
   messages: messages,
 })
--- src/classes/utilities/LocaleUtil.ts
+++ src/classes/utilities/LocaleUtil.ts
@@ -1,10 +1,15 @@
 import { Locale } from "vue-i18n"
 import i18n from '../setup/i18n'
+import { useCookies } from 'vue3-cookies'
 
 export function useLocale() {
+  const { cookies } = useCookies()
+
   const switchLocale = (locale: Locale) => {
     // FIXME: 本来であれば $i18n のグローバル変数を支えるはずなのだが…
     i18n.global.locale.value = locale
+
+    cookies.set('i18n_locale', locale)
   }
 
   return {
