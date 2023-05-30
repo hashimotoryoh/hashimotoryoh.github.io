5589d6d39d127bb5b18da9f466f8a01239370a38
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-05-29 21:43:09 -0700

    Create manifest.webmanifest by vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite"
 
 // https://vitejs.dev/config/
@@ -24,6 +25,25 @@ export default defineConfig({
         '@vueuse/head',
       ],
       dts: './src/auto-imports.d.ts',
+    }),
+    VitePWA({
+      manifest: {
+        id: 'ryohs-link-in-bio',
+        name: 'Ryoh\'s Link in Bio',
+        short_name: 'Ryoh\'s LiB',
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
     VueI18nPlugin({
       include: `${__dirname}/src/assets/locales/**`,
