6f4142132e029e25f5d815067c2d78c14a7038fd
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2023-05-31 16:25:33 -0700

    Instagramのフィードをコンポーネント化

---
+++ src/components/InstagramFeed.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import links from '@/assets/json/sns.json'
+import media from '@/assets/json/instagram_media.json'
+
+const NUM_OF_MEDIA_TO_VIEW = 9
+
+const mediaToView = media.slice(0, NUM_OF_MEDIA_TO_VIEW)
+const instagramLink = links.find(i => i.name === 'Instagram')
+</script>
+
+<template>
+  <ul class="grid grid-cols-3 gap-1">
+    <li
+      v-for="theMedia in mediaToView"
+      :key="theMedia.id"
+      class="aspect-square grayscale relative"
+    >
+      <a
+        :href="theMedia.permalink.toString()"
+        target="_blank"
+      >
+        <img
+          :src="theMedia.media_url.toString()"
+          :alt="theMedia.caption"
+          class="w-full h-full object-cover rounded pointer-events-none"
+        >
+        <span
+          v-if="theMedia.media_type === 'CAROUSEL_ALBUM'"
+          class="absolute top-2 right-2 -rotate-90 text-white text-lg"
+        >
+          <Icon icon="mdi:checkbox-multiple-blank" />
+        </span>
+      </a>
+    </li>
+  </ul>
+  <div class="my-2 text-center">
+    <a
+      v-if="instagramLink"
+      :href="instagramLink?.url"
+      target="_blank"
+      class="text-slate-400"
+    >
+      {{ $t('pages.index.see_more') }}
+    </a>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import links from '@/assets/json/sns.json'
-import media from '@/assets/json/instagram_media.json'
 import InlineSvg from 'vue-inline-svg'
-
-const NUM_OF_MEDIA_TO_VIEW = 9
-
-const mediaToView = media.slice(0, NUM_OF_MEDIA_TO_VIEW)
-const instagramLink = links.find(i => i.name === 'Instagram')
+import InstagramFeed from '@/components/InstagramFeed.vue'
 
 defineComponent({
   InlineSvg
@@ -69,38 +64,7 @@ defineComponent({
     <hr class="my-8 mx-1">
 
     <div class="max-w-screen-md mx-auto my-8 px-1">
-      <ul class="grid grid-cols-3 gap-1">
-        <li
-          v-for="theMedia in mediaToView"
-          :key="theMedia.id"
-          class="aspect-square grayscale relative"
-        >
-          <a
-            :href="theMedia.permalink.toString()"
-            target="_blank"
-          >
-            <img
-              :src="theMedia.media_url.toString()"
-              :alt="theMedia.caption"
-              class="w-full h-full object-cover rounded pointer-events-none"
-            >
-            <span
-              v-if="theMedia.media_type === 'CAROUSEL_ALBUM'"
-              class="absolute top-2 right-2 -rotate-90 text-white text-lg"
-            >
-              <Icon icon="mdi:checkbox-multiple-blank" />
-            </span>
-          </a>
-        </li>
-      </ul>
-      <div class="my-2 text-center">
-        <a
-          v-if="instagramLink"
-          :href="instagramLink?.url"
-          target="_blank"
-          class="text-slate-400"
-        >{{ $t('pages.index.see_more') }}</a>
-      </div>
+      <InstagramFeed />
     </div>
   </div>
 </template>
